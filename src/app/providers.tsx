d98bc2c4f7117dbee2f98cee5dafd5c539b44b7a
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-11 23:33:40 +0900

    nextuiの導入

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
+import { useRouter } from 'next/navigation';
+import type { ReactNode } from 'react';
+
+export function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <NextUIProvider navigate={router.push}>
+      <ThemeProvider attribute='class' defaultTheme='dark'>
+        {children}
+      </ThemeProvider>
+    </NextUIProvider>
+  );
+}
