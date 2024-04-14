f0eefd4686e0d5f26818e3266f74b503d2159b70
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-14 21:05:37 +0900

    フォーム用コンポーネントの追加

---
+++ src/components/ui/selects/channel-select.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import type { GuildChannel } from '@/@types/discord';
+import { FormSelectClassNames } from '@/components/form';
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
+import { Chip } from '@nextui-org/chip';
+import {
+  Select,
+  SelectItem,
+  type SelectProps,
+  type SelectedItems,
+} from '@nextui-org/select';
+import { ChannelType } from 'discord-api-types/v10';
+
+const channelTypeIcons = new Map<ChannelType, string>([
+  [ChannelType.GuildAnnouncement, 'solar:mailbox-bold'],
+  [ChannelType.AnnouncementThread, 'solar:hashtag-chat-bold'],
+  [ChannelType.GuildCategory, 'solar:folder-2-bold'],
+  [ChannelType.GuildForum, 'solar:chat-round-bold'],
+  [ChannelType.GuildMedia, 'solar:gallery-minimalistic-bold'],
+  [ChannelType.GuildStageVoice, 'solar:translation-2-bold'],
+  [ChannelType.GuildText, 'solar:hashtag-bold'],
+  [ChannelType.GuildVoice, 'solar:volume-loud-bold'],
+  [ChannelType.PublicThread, 'solar:hashtag-chat-bold'],
+  [ChannelType.PrivateThread, 'solar:hashtag-chat-bold'],
+]);
+
+type Props = {
+  channels: GuildChannel[];
+  types?: ChannelType[];
+  selectionMode?: keyof typeof FormSelectClassNames;
+} & Omit<
+  SelectProps,
+  'children' | 'renderValue' | 'items' | 'placeholder' | 'selectionMode'
+>;
+
+export function ChannelSelect({
+  classNames,
+  channels,
+  types,
+  isRequired,
+  selectionMode = 'single',
+  ...props
+}: Props) {
+  const sortedChannels = channels
+    .filter((channel) => (types ? types.includes(channel.type) : true))
+    .sort((a, b) => a.position - b.position);
+
+  function renderValue(items: SelectedItems<GuildChannel>) {
+    return (
+      <div className='flex flex-wrap items-center gap-1'>
+        {items.map((item) =>
+          selectionMode === 'multiple' ? (
+            <MultipleSelectItem channel={item.data} key={item.key} />
+          ) : (
+            <SingleSelectItem channel={item.data} key={item.key} />
+          ),
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <Select
+      classNames={classNames ?? FormSelectClassNames[selectionMode]}
+      items={sortedChannels}
+      variant='bordered'
+      placeholder='チャンネルを選択'
+      renderValue={renderValue}
+      selectionMode={selectionMode}
+      isMultiline={selectionMode === 'multiple'}
+      isRequired={isRequired}
+      disallowEmptySelection={isRequired}
+      {...props}
+    >
+      {(channel) => (
+        <SelectItem
+          key={channel.id}
+          value={channel.id}
+          textValue={channel.name}
+        >
+          <SingleSelectItem channel={channel} />
+        </SelectItem>
+      )}
+    </Select>
+  );
+}
+
+function SingleSelectItem({ channel }: { channel?: GuildChannel | null }) {
+  return (
+    <div className='flex items-center gap-2 text-default-500'>
+      <Icon
+        // biome-ignore lint/style/noNonNullAssertion: <explanation>
+        icon={channelTypeIcons.get(channel?.type!) || 'solar:hashtag-chat-bold'}
+      />
+      <span className='text-foreground'>{channel?.name}</span>
+    </div>
+  );
+}
+
+function MultipleSelectItem({ channel }: { channel?: GuildChannel | null }) {
+  return <Chip>{channel?.name}</Chip>;
+}
---
+++ src/components/ui/selects/role-select.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { FormSelectClassNames } from '@/components/form';
+import { Chip } from '@nextui-org/chip';
+import {
+  Select,
+  SelectItem,
+  type SelectProps,
+  type SelectedItems,
+} from '@nextui-org/select';
+import type { APIRole } from 'discord-api-types/v10';
+
+export type Props = {
+  roles: APIRole[];
+  filter?: (role: APIRole) => boolean;
+  selectionMode?: keyof typeof FormSelectClassNames;
+} & Omit<
+  SelectProps,
+  'children' | 'renderValue' | 'items' | 'placeholder' | 'selectionMode'
+>;
+
+export function RoleSelect({
+  classNames,
+  roles,
+  filter,
+  isRequired,
+  selectionMode = 'single',
+  ...props
+}: Props) {
+  const sortedRoles = roles
+    .filter((role) => (filter ? filter(role) : true))
+    .sort((a, b) => (a.position < b.position ? 1 : -1));
+
+  function renderValue(items: SelectedItems<APIRole>) {
+    return (
+      <div className='flex flex-wrap gap-1'>
+        {items.map((item) =>
+          selectionMode === 'multiple' ? (
+            <MultipleSelectItem role={item.data} key={item.key} />
+          ) : (
+            <SingleSelectItem role={item.data} key={item.key} />
+          ),
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <Select
+      classNames={classNames ?? FormSelectClassNames[selectionMode]}
+      items={sortedRoles}
+      variant='bordered'
+      placeholder='ロールを選択'
+      renderValue={renderValue}
+      selectionMode={selectionMode}
+      isMultiline={selectionMode === 'multiple'}
+      isRequired={isRequired}
+      disallowEmptySelection={isRequired}
+      {...props}
+    >
+      {(role) => (
+        <SelectItem key={role.id} value={role.id} textValue={role.name}>
+          <SingleSelectItem role={role} />
+        </SelectItem>
+      )}
+    </Select>
+  );
+}
+
+function SingleSelectItem({ role }: { role?: APIRole | null }) {
+  return (
+    <div className='flex items-center gap-2'>
+      <div className='flex size-[18px] items-center justify-center'>
+        <div
+          className='h-2 w-2 rounded-full'
+          style={{
+            backgroundColor: role?.color
+              ? `#${role.color.toString(16)}`
+              : 'GrayText',
+          }}
+        />
+      </div>
+      <span className='text-foreground'>{role?.name}</span>
+    </div>
+  );
+}
+
+function MultipleSelectItem({ role }: { role?: APIRole | null }) {
+  return <Chip>{role?.name}</Chip>;
+}
