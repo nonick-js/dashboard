f82454f6496d3f19e837b4cd9a33250517150a1c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-04-17 21:27:51 +0900

    nprogress routerの置き換え

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,14 @@
 
 import { CheckSessionProvider } from '@/components/providers/check-session';
 import { ConsoleWarningProvider } from '@/components/providers/console-warn';
-import { usePRouter } from '@/hooks/use-prouter';
 import { NextUIProvider } from '@nextui-org/react';
 import { SessionProvider } from 'next-auth/react';
+import { useRouter } from 'next-nprogress-bar';
 import { ThemeProvider } from 'next-themes';
-import { type ReactNode, Suspense } from 'react';
+import type { ReactNode } from 'react';
 
 export function Providers({ children }: { children: ReactNode }) {
-  const router = usePRouter();
+  const router = useRouter();
 
   return (
     <SessionProvider>
