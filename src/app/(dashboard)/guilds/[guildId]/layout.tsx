1b20cb2a42d69cf4ff407663858f7836f858c4c3
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-05-30 09:52:05 +0900

    ナビゲーションバーを独立

--- src/app/(dashboard)/guilds/[guildId]/layout.tsx
+++ src/app/(dashboard)/guilds/[guildId]/layout.tsx
@@ -1,6 +1,5 @@
-import { ScrollShadow } from '@nextui-org/scroll-shadow';
 import type { ReactNode } from 'react';
-import { NavBar } from '../../navbar';
+import { NavBar } from './navbar';
 import { Sidebar } from './sidebar';
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -8,11 +7,9 @@ export default function Layout({ children }: { children: ReactNode }) {
     <>
       <NavBar />
       {/* 影描写のため、モバイルデバイスの場合はcontainerのPaddingではなくパネルのPaddingを使う */}
-      <div className='max-lg:px-0 container justify-center flex h-[calc(100dvh_-_80px)]'>
+      <div className='max-lg:px-0 container flex justify-center'>
         <Sidebar />
-        <ScrollShadow className='flex-1' hideScrollBar>
-          <div className='flex flex-col gap-6 px-[2rem] lg:pl-8 lg:pr-6'>{children}</div>
-        </ScrollShadow>
+        <div className='flex-1 flex flex-col gap-6 px-[2rem] lg:pl-8 lg:pr-6'>{children}</div>
       </div>
     </>
   );
