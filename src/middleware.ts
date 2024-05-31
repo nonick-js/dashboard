272f756cef6d41fb6ad7921a3f9c5c330edc5151
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-05-31 09:34:47 +0900

    prefetch時にmiddlewareが読み込まれないよう変更

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,24 @@ export default auth(async (req) => {
 
   if (req.nextUrl.pathname.startsWith('/login')) {
     if (isAuthenticated) return NextResponse.redirect(new URL('/', req.url));
-    return;
+    return NextResponse.next();
   }
 
   if (!isAuthenticated) {
     let from = req.nextUrl.pathname;
     if (req.nextUrl.search) from += req.nextUrl.search;
-    return NextResponse.redirect(
-      new URL(`/login?from=${encodeURIComponent(from)}`, req.url),
-    );
+    return NextResponse.redirect(new URL(`/login?from=${encodeURIComponent(from)}`, req.url));
   }
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|manifest).*)'],
+  matcher: [
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico|manifest).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
 };
