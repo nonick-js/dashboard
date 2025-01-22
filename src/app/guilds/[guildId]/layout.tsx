3a2310a624fbe71412333aae64475b3e8ab59ee2
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-01-22 15:11:27 +0900

    feat: サイドバーにLoading UIを追加

--- src/app/guilds/[guildId]/layout.tsx
+++ src/app/guilds/[guildId]/layout.tsx
@@ -1,20 +1,22 @@
 ﻿import { getGuild } from '@/lib/discord';
-import type { ReactNode } from 'react';
+import { type ReactNode, Suspense } from 'react';
 import { Navbar } from './navbar';
 import { Sidebar } from './sidebar';
 import { SidebarProvider } from './sidebar-provider';
+import { SidebarSkeleton } from './sidebar-skeleton';
 
 export default async function Layout({
   children,
   params,
 }: { children: ReactNode; params: Promise<{ guildId: string }> }) {
   const { guildId } = await params;
-  const guild = await getGuild(guildId);
 
   return (
     <SidebarProvider>
       <div className='flex'>
-        <Sidebar guild={guild} />
+        <Suspense fallback={<SidebarSkeleton />}>
+          <FetchSidebar guildId={guildId} />
+        </Suspense>
         <div className='flex-1 flex-col'>
           <Navbar />
           <main className='p-6 pt-0'>{children}</main>
@@ -22,4 +24,10 @@ export default async function Layout({
       </div>
     </SidebarProvider>
   );
+}
+
+async function FetchSidebar({ guildId }: { guildId: string }) {
+  const guild = await getGuild(guildId);
+
+  return <Sidebar guild={guild} />;
 }
---
+++ src/app/guilds/[guildId]/sidebar-skeleton.tsx
@@ -0,0 +1,81 @@
+﻿import { Logo } from '@/components/logo';
+import { Button } from '@heroui/button';
+import { Link } from '@heroui/link';
+import { Skeleton } from '@heroui/skeleton';
+import { Spacer } from '@heroui/spacer';
+import { listbox, listboxSection } from '@heroui/theme';
+import type { ReactNode } from 'react';
+
+export function SidebarSkeleton() {
+  return (
+    <div className='sticky top-0 border-r-1 border-divider'>
+      <div className='w-[300px] h-dvh p-6 pt-0'>
+        <div className='h-[80px] flex shrink-0 items-center px-1'>
+          <Link href='/'>
+            <Logo height={16} />
+          </Link>
+        </div>
+
+        <Button
+          as={Link}
+          href='/'
+          className='flex flex-shrink-0 h-14 font-semibold justify-between'
+          variant='bordered'
+          isDisabled
+        >
+          <div className='flex items-center gap-2 overflow-hidden'>
+            <Skeleton className='flex-shrink-0 w-8 h-8 rounded-full' />
+            <Skeleton className='w-36 h-4 rounded-md' />
+          </div>
+        </Button>
+
+        <Spacer y={3} />
+
+        <ListboxSkeleton>
+          <ListboxSectionSkeleton>
+            {Array.from({ length: 2 }).map((_, index) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              <ListboxItemSkeleton key={index} />
+            ))}
+          </ListboxSectionSkeleton>
+          <ListboxSectionSkeleton>
+            {Array.from({ length: 8 }).map((_, index) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              <ListboxItemSkeleton key={index} />
+            ))}
+          </ListboxSectionSkeleton>
+        </ListboxSkeleton>
+      </div>
+    </div>
+  );
+}
+
+function ListboxSkeleton({ children }: { children: ReactNode }) {
+  const listboxClasses = listbox();
+
+  return (
+    <div className={listboxClasses.base()}>
+      <nav className={listboxClasses.list()}>{children}</nav>
+    </div>
+  );
+}
+
+function ListboxSectionSkeleton({ children }: { children: ReactNode }) {
+  const listboxSectionClasses = listboxSection();
+
+  return (
+    <li className={listboxSectionClasses.base({ className: 'w-full list-none' })}>
+      <Skeleton className='ml-1 mt-2 h-4 w-12 rounded-small' />
+      <ul className={listboxSectionClasses.group({ className: 'space-y-0.5 pt-1' })}>{children}</ul>
+    </li>
+  );
+}
+
+function ListboxItemSkeleton() {
+  return (
+    <div className='flex items-center gap-2 px-3 py-1.5 h-[44px]'>
+      <Skeleton className='w-6 h-6 rounded-small' />
+      <Skeleton className='flex-1 w-full h-4 rounded-md' />
+    </div>
+  );
+}
