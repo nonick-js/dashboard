450a97e218ee932dee678199fd5a18b3dca0dfc7
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-01-27 11:09:51 +0900

    feat: 自動アナウンス公開の設定ページを実装

---
+++ src/app/guilds/[guildId]/(features)/auto-public/action.ts
@@ -0,0 +1,20 @@
+﻿'use server';
+
+import { AutoPublicModel } from '@/lib/database/mongoose';
+import { AutoPublicZodSchema } from '@/lib/database/zod';
+import { ActionType, dashboardActionClient } from '@/lib/safe-action';
+import { revalidatePath } from 'next/cache';
+
+export const updateSetting = dashboardActionClient
+  .schema(async (prevSchema) => prevSchema.and(AutoPublicZodSchema))
+  .metadata({ actionType: ActionType.updateSetting })
+  .action(async ({ parsedInput }) => {
+    await AutoPublicModel.updateOne(
+      { guildId: parsedInput.guildId },
+      { $set: parsedInput },
+      { upsert: true },
+    );
+
+    revalidatePath('/');
+    return { success: true };
+  });
