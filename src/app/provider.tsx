219415419ce06dfb33e91f54b918bcb77804ff1e
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-02-26 17:37:58 +0900

    feat: herouiの導入

---
+++ src/app/provider.tsx
@@ -0,0 +1,15 @@
+﻿'use client';
+
+import { HeroUIProvider } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+
+export function Provider({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <HeroUIProvider navigate={router.push}>
+      {children}
+    </HeroUIProvider>
+  )
+}
