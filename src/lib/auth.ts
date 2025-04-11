e6420c600b4f63bc4c91f9c72f93814e76019f70
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-04-12 01:16:42 +0900

    feat: sessionに含まれるuserにユーザ名と識別子を追加

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,35 @@
 ﻿import 'server-only';
 
 import type { RESTPostOAuth2AccessTokenResult } from 'discord-api-types/v10';
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import type { JWT } from 'next-auth/jwt';
-import discord from 'next-auth/providers/discord';
+import discord, { type DiscordProfile } from 'next-auth/providers/discord';
+import { NextResponse } from 'next/server';
 import { discordFetch } from './discord/fetch';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     discord({
       authorization: 'https://discord.com/api/oauth2/authorize?scope=identify+guilds',
+      profile: (profile: DiscordProfile) => {
+        if (profile.avatar === null) {
+          const defaultAvatarNumber =
+            profile.discriminator === '0'
+              ? Number(BigInt(profile.id) >> BigInt(22)) % 6
+              : Number.parseInt(profile.discriminator) % 5;
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith('a_') ? 'gif' : 'png';
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
+        return {
+          id: profile.id,
+          image: profile.image_url,
+          name: profile.global_name ?? profile.username,
+          username: profile.username,
+          discriminator: profile.discriminator,
+        };
+      },
     }),
   ],
   pages: {
@@ -25,6 +45,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           refreshToken: account.refresh_token as string,
           expiresAt: account.expires_at as number,
         } as JWT;
+    jwt: async ({ token, account, user }) => {
+      if (account && user) {
+        token.userId = account.providerAccountId;
+        token.username = user.username;
+        token.discriminator = user.discriminator;
+        token.accessToken = account.access_token as string;
+        token.refreshToken = account.refresh_token as string;
+        token.expiresAt = account.expires_at as number;
+        return token;
       }
 
       if (Date.now() < token.expiresAt * 1000) return token;
@@ -45,15 +74,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         return token;
       }
 
-      return {
-        ...token,
-        accessToken: data.access_token,
-        refreshToken: data.refresh_token,
-        expiresAt: Math.floor(Date.now() / 1000 + data.expires_in),
-      };
+      token.accessToken = data.access_token;
+      token.refreshToken = data.refresh_token;
+      token.expiresAt = Math.floor(Date.now() / 1000 + data.expires_in);
+      return token;
     },
     session: ({ session, token }) => {
       session.user.id = token.userId;
+      session.user.username = token.username;
+      session.user.discriminator = token.discriminator;
       session.user.accessToken = token.accessToken;
       session.error = token.error;
       return session;
@@ -62,12 +91,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 });
 
 declare module 'next-auth' {
+  interface User {
+    username: string;
+    discriminator: string;
+  }
   interface Session {
-    user: {
-      accessToken: string;
+    user: DefaultSession['user'] & {
       id: string;
       name: string;
       image: string;
+      accessToken: string;
     };
     error?: 'RefreshTokenError';
   }
@@ -75,10 +108,15 @@ declare module 'next-auth' {
 
 declare module 'next-auth/jwt' {
   interface JWT {
+    // user
+    userId: string;
+    username: string;
+    discriminator: string;
+
+    // accounts
     accessToken: string;
     refreshToken: string;
     expiresAt: number;
-    userId: string;
     error?: 'RefreshTokenError';
   }
 }
