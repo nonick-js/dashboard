ed5c8f840b451341e90ecda5067bff05d4f4b383
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-04-13 01:17:20 +0900

    feat: サーバー選択ページにmetadataを追加

--- src/app/(guild-select)/page.tsx
+++ src/app/(guild-select)/page.tsx
@@ -1,7 +1,12 @@
 import { auth } from '@/lib/auth';
 import { getMutualManagedGuilds } from '@/lib/discord/api';
+import type { Metadata } from 'next';
 import { GuildCardContainer } from './guild-container';
 import { SessionAlert } from './session-alert';
+
+export const metadata: Metadata = {
+  title: 'サーバー選択',
+};
 
 export default async function Page() {
   const session = await auth();
