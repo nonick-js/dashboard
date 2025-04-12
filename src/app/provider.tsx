73c86f457193d039a7f594e8e58d1e2af422f87b
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-04-12 23:20:57 +0900

    feat: ブラウザのコンソールを開いた際の警告メッセージを追加

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,10 +4,22 @@ import { HeroUIProvider } from '@heroui/react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { useRouter } from 'next/navigation';
-import type { ReactNode } from 'react';
+import { type ReactNode, useEffect } from 'react';
 
 export function Provider({ children }: { children: ReactNode }) {
   const router = useRouter();
+
+  useEffect(() => {
+    console.log('%cストップ！', 'font-size: 4rem; color: red');
+    console.log(
+      '%cここに何かを貼り付けると、あなたのアカウントが危険にさらされる可能性があります!',
+      'font-size: 1rem; color: orange',
+    );
+    console.log(
+      '%c自分が何をしようとしているか理解していないのなら、何も入力せずウィンドウを閉じるべきです。',
+      'font-size: 1rem',
+    );
+  }, []);
 
   return (
     <SessionProvider refetchOnWindowFocus={false}>
