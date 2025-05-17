8556f799b887bb7b8a361478ec9f1d9729339f13
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-05-17 08:19:13 +0000

    feat: colorpicker, colorinputの追加

---
+++ src/components/react-hook-form/ui/color-picker.tsx
@@ -0,0 +1,38 @@
+﻿import { useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
+import {
+  type FieldPath,
+  type FieldValues,
+  type UseControllerProps,
+  useController,
+} from 'react-hook-form';
+import { useDebouncyEffect } from 'use-debouncy';
+
+const numberToHex = (n: number): string => `#${n.toString(16).padStart(6, '0')}`;
+const hexToNumber = (hex: string): number => Number.parseInt(hex.replace('#', ''), 16);
+
+export function ControlledHexColorPicker<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({ control, name }: UseControllerProps<TFieldValues, TName>) {
+  const { field } = useController({ name, control });
+
+  const initialHex = numberToHex(field.value ?? 0);
+  const [hexColor, setHexColor] = useState<string>(initialHex);
+
+  useDebouncyEffect(
+    () => {
+      const numeric = hexToNumber(hexColor);
+      field.onChange(numeric);
+    },
+    100,
+    [hexColor],
+  );
+
+  return (
+    <HexColorPicker
+      color={numberToHex(field.value ?? 0)}
+      onChange={(newHex) => setHexColor(newHex)}
+    />
+  );
+}
