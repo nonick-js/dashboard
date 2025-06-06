950a3a57b29764fcb842a0cd1626c812c82a89e5
nonick <87268971+nonick-mc@users.noreply.github.com>
2025-06-06 00:43:28 +0000

    fix: サーバー内通報の設定フォーム用のZodスキーマが以前の参照方法を取っていた問題を修正

--- src/app/(app)/guilds/[guildId]/report/schema.ts
+++ src/app/(app)/guilds/[guildId]/report/schema.ts
@@ -1,8 +1,8 @@
-﻿import { createInsertSchema } from '@/lib/database/src/lib/drizzle';
-import { reportSetting } from '@/lib/database/src/schema/setting';
-import { isUniqueArray } from '@/lib/database/src/utils/zod';
-import { snowflakeRegex } from '@/lib/database/src/utils/zod/discord';
+﻿import { reportSetting } from '@/lib/database/src/schema/setting';
+import { snowflakeRegex } from '@/lib/zod/discord/constants';
+import { createInsertSchema } from '@/lib/zod/drizzle';
 import { z } from '@/lib/zod/i18n';
+import { isUniqueArray } from '@/lib/zod/utils';
 
 export const settingFormSchema = createInsertSchema(reportSetting, {
   channel: (schema) => schema.regex(snowflakeRegex),
